3cc79e82bdbbc29ee9faad2b8a1d12ad4bad12e2
Pretender <DmitrijDDD@gmail.com>
2013-10-11 09:46:45 +0300

    iterators

--- src/Main.java
+++ src/Main.java
@@ -25,13 +25,27 @@ public class Main {
      */
     public static void main(String[] args) {
         try {
-            File file = new File("in.xml");
+            File file = new File("In.xml");
             JAXBContext jaxbContext = JAXBContext.newInstance(Soft.class);
             Unmarshaller um = jaxbContext.createUnmarshaller();
             Soft soft = (Soft) um.unmarshal(file);
-            /*for (int i=0;i<soft.company.)  */
 
+            Integer sys=0;
+            Integer ed=0;
+            for(Company company:soft.getCompany()){
 
+                for (Product product:company.getSystem()){
+                    sys+=product.getPrice();
+                }
+
+                for (Product product:company.getEditor()){
+                    ed+=product.getPrice();
+                }
+            }
+
+            System.out.println("Summ system = "+sys);
+            System.out.println("Summ editor = "+ed);
+            System.out.println("All summ = "+(ed+sys));
             System.out.println(soft);
         } catch (Exception e) {
             e.printStackTrace();
